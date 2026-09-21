1b8cc5f8f2b679fdffbc3abb2031a1969c4b5a79
Tvoje Hnizdo <dveruce@gmail.com>
2026-09-21 18:01:53 +0200

    Pin stav_impulse.ps1 to canonical RV V2 database

--- stav_impulse.ps1
+++ stav_impulse.ps1
@@ -1,5 +1,6 @@
 $ErrorActionPreference = "Stop"
 Set-Location $PSScriptRoot
+$env:RV_DB = "data/relative_value_v2.db"
 
 $BaseUrl = "http://127.0.0.1:8765"
 $OutDir = Join-Path $PSScriptRoot "stav_reporty"
